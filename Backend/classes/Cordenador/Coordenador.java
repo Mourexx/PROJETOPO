ccb96e9d98cde79f98fdd5a1c5a8693fa92c84d0
alicess99 <alicesousasilva1819@gmail.com>
2025-12-03 16:58:17 +0000

    Update Coordenador.java

--- Backend/classes/Cordenador/Coordenador.java
+++ Backend/classes/Cordenador/Coordenador.java
@@ -1,15 +1,17 @@
 package Coordenador;
 
+import java.io.Serializable;  // Importar a interface Serializable
 
-public class Coordenador {
+public class Coordenador implements Serializable {
 
-    
+    private static final long serialVersionUID = 1L;  // Versão da serialização, importante para compatibilidade entre versões
+
     private int idCoordenador;      
     private String nomeCompleto;    
     private String email;            
     private int numProjetosGere;     
 
-   
+    // Construtor
     public Coordenador(int idCoordenador, String nomeCompleto, String email) {
         this.idCoordenador = idCoordenador;
         this.nomeCompleto = nomeCompleto;
@@ -22,63 +24,4 @@ public class Coordenador {
         return idCoordenador;
     }
 
-    public void setIdCoordenador(int idCoordenador) {
-        this.idCoordenador = idCoordenador;
-    }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getNumProjetosGere() {
-        return numProjetosGere;
-    }
-
-    public void setNumProjetosGere(int numProjetosGere) {
-        this.numProjetosGere = numProjetosGere;
-    }
-
-   
-    public void autenticar() {
-        
-        System.out.println("Autenticação realizada com sucesso para o coordenador: " + nomeCompleto);
-    }
-
-    public void criarProjeto() {
-       
-        System.out.println("Projeto criado pelo coordenador: " + nomeCompleto);
-        numProjetosGere++; 
-    }
-
-    public void adicionarInvestigadorAoProjeto() {
-        
-        System.out.println("Investigador adicionado ao projeto.");
-    }
-
-    public void removerInvestigadorDoProjeto() {
-        
-        System.out.println("Investigador removido do projeto.");
-    }
-
-    public void registarAtividade() {
-       
-        System.out.println("Atividade registada com sucesso.");
-    }
-
-    public void acompanharProgresso() {
-      
-        System.out.println("Progresso do projeto monitorado.");
-    }
-}
+    public void setIdCoordenado
